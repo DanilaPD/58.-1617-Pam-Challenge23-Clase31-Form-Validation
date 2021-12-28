48b2278ed1fb1d7ff786f88afb5060357f1e1750
unknown <danila.perezdiaz@gmail.com>
2021-12-28 15:31:50 -0300

    Form Validation

--- index.js
+++ index.js
@@ -11,18 +11,66 @@ form.addEventListener("submit", (e) => {
   validate();
 });
 
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i;
+
+const password1Regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+favInstrument.selectedIndex = -1;
+
 const validate = () => {
   console.log("Here, validating the form!");
-  const user = username.value.trim();
+  const userName = username.value.trim();
+  const mailAccount = email.value.trim();
+  const pass1 = password1.value.trim();
+  const pass2 = password2.value.trim();
+  const instrument = favInstrument.value;
 
-  if (user === "") {
+  if (userName === "") {
     let errorMessage = "This space cannot remain empty!";
     inputError(username, errorMessage);
-  } else if (user.length < 2 || user.length > 30) {
+  } else if (userName.length < 2 || userName.length > 30) {
     let errorMessage = "The username must have between 2 and 30 characters!";
     inputError(username, errorMessage);
   } else {
     inputSuccess(username);
+  }
+
+  if (mailAccount === "") {
+    let errorMessage = "This space cannot remain empty!";
+    inputError(email, errorMessage);
+  } else if (!emailRegex.test(mailAccount)) {
+    let errorMessage = "This email account is not valid!";
+    inputError(email, errorMessage);
+  } else {
+    inputSuccess(email);
+  }
+
+  if (instrument === "") {
+    let errorMessage = "Select an instrument!";
+    inputError(favInstrument, errorMessage);
+  } else {
+    inputSuccess(favInstrument);
+  }
+
+  if (pass1 === "") {
+    let errorMessage = "This space cannot remain empty!";
+    inputError(password1, errorMessage);
+  } else if (!password1Regex.test(pass1)) {
+    let errorMessage =
+      "This is not a valid password. It should have, at least, 8 characters, one upper case letter, one lower case letter and two numbers.";
+    inputError(password1, errorMessage);
+  } else {
+    inputSuccess(password1);
+  }
+
+  if (pass2 === "") {
+    let errorMessage = "This space cannot remain empty!";
+    inputError(password2, errorMessage);
+  } else if (pass2 !== pass1) {
+    let errorMessage = "The passwords do not coincide.";
+    inputError(password2, errorMessage);
+  } else {
+    inputSuccess(password2);
   }
 };
 
